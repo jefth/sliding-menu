16305a5f901c4da0130367de1658579220247703
chenyoca <chenyoca@gmail.com>
2013-02-26 17:07:23 +0800

    修正Sample项目

--- example/src/com/slidingmenu/example/fragments/ResponsiveUIActivity.java
+++ example/src/com/slidingmenu/example/fragments/ResponsiveUIActivity.java
@@ -7,7 +7,6 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
-import com.actionbarsherlock.view.MenuItem;
 import com.slidingmenu.example.R;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.app.SlidingFragmentActivity;
@@ -40,7 +39,7 @@ public class ResponsiveUIActivity extends SlidingFragmentActivity {
 			getSlidingMenu().setSlidingEnabled(true);
 			getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 			// show home as up so we can toggle
-			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//			getActionBar().setDisplayHomeAsUpEnabled(true);
 		} else {
 			// add a dummy view
 			View v = new View(this);
@@ -81,14 +80,14 @@ public class ResponsiveUIActivity extends SlidingFragmentActivity {
 			.show();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			toggle();
-		}
-		return super.onOptionsItemSelected(item);
-	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		switch (item.getItemId()) {
+//		case android.R.id.home:
+//			toggle();
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
 
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
