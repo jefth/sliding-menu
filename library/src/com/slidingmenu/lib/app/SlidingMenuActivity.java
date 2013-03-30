f01478d36978a2e02a9967084ae288be14c5d50e
chenyoca <chenyoca@gmail.com>
2013-03-30 11:19:38 +0800

    Change:更新SupportV4到最新版本，添加SlidingMenuActivity

---
+++ library/src/com/slidingmenu/lib/app/SlidingMenuActivity.java
@@ -0,0 +1,45 @@
+package com.slidingmenu.lib.app;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.slidingmenu.lib.SlidingMenu;
+
+public abstract class SlidingMenuActivity extends SlidingFragmentActivity {
+
+	protected SlidingMenu slidingMenu;
+	protected View contentView;
+	protected View sidebarLeftView;
+	protected View sidebarRightView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		slidingMenu  = getSlidingMenu();
+		
+		onConfigSlidingMenu(slidingMenu);
+		
+		contentView = onCreateContentView();
+		sidebarLeftView = onCreateLeftView();
+		sidebarRightView = onCreateRightView();
+		
+		setContentView(contentView);
+		setBehindContentView(sidebarLeftView);
+		if (sidebarRightView != null){
+			slidingMenu.setSecondaryMenu(sidebarRightView);
+		}
+	}
+	
+	// configuration of sliding menu
+	public abstract void onConfigSlidingMenu(SlidingMenu slidingMenu);
+	
+	// create Main content view
+	public abstract View onCreateContentView();
+	
+	// create Left side view
+	public abstract View onCreateLeftView();
+	
+	// create Right side view
+	public abstract View onCreateRightView();
+
+}
