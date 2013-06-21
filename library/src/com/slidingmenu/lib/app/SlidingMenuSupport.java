3cbab9f62c08aae2ec3c3fe08952362d7a099781
Yoojia.chen <chenyoca@gmail.com>
2013-06-21 14:54:55 +0800

    Add Sliding menu support interface

---
+++ library/src/com/slidingmenu/lib/app/SlidingMenuSupport.java
@@ -0,0 +1,13 @@
+package com.slidingmenu.lib.app;
+
+import com.slidingmenu.lib.SlidingMenu;
+
+/**
+ * author : 桥下一粒砂 (chenyoca@gmail.com)
+ * date   : 2013-06-21
+ * TODO
+ */
+public interface SlidingMenuSupport {
+
+	void addSupportSlidingMenu(SlidingMenu slidingMenu);
+}
